ddfb18bc97cf1cd0aef28410e7095118d2651ac4
Jake4077 <57727352+Jake4077@users.noreply.github.com>
2019-12-18 09:44:08 -0600

    This is the final project
    Pygame is now working. There may only be one room, but all other main functions are working with Pygame initializing, opening, and closing for each image that is desired to display.

--- Final Project.py
+++ Final Project.py
@@ -12,23 +12,46 @@ class User():
         self.difficulty = difficulty
         self.room = room
 
-#creates a new game
+#creates a new game (Working)
 def newgame():
-    user_name = input("Please enter your name here:")
-    difficulty = input("Please enter a difficulty level here (easy, medium, hard, impossible):")
-    return user_name, difficulty
+    global player
+    user_name = input("Please enter your name here: ")
+    difficulty = input("Please enter a difficulty level here (easy, medium, hard, impossible): ")
+    health = starting_health(difficulty)
+    player = User(user_name, health, difficulty, 0, 0, 1)
+    return player
 
-#start of the program
+#start of the program (WORKING, ADD ROOMS AS IT GOES)
 def start():
     print("Do you wish to load a saved game or start a new one?")
-    action = input()
+    action = input("> ")
     """If statement to decide if loading or creating a new game"""
-    if load in action:
-        loadgame()
-    elif new_game in action:
+    for x in load:
+        if x == action:
+            loadgame('save.bin')
+            loaded_user = loadgame('save.bin')
+            room_num = loaded_user.room
+            if room_num == 1:
+                room1()
+            elif room_num == 2:
+                room2()
+            elif room_num == 3:
+                room3()
+            elif room_num == 4:
+                room4()
+            else:
+                print("error finding save file")
+        else:
+            pass
+    if action == 'new':
         newgame()
+        player.room == 1
+        room1()
+    else:
+        print("Better select 'new' or 'load'")
+        start()
 
-#determination of difficulty
+#determination of difficulty (WORKING)
 def starting_health(difficulty):
     while starting_health != 1 or starting_health !=3 or starting_health != 5 or starting_health != 7:
         if difficulty == "easy":
@@ -42,17 +65,91 @@ def starting_health(difficulty):
         else:
             difficulty = input("Please enter difficulty as exactly 'easy', 'medium', 'hard', or 'impossible':")
 
-#function to save game
-def savegame():
-    with open('savefile', 'w') as f:
-        pickle.dump(f)
+#function to save game (WORKING)
+def savegame(filename):
+    file_out = open(filename, 'wb')
+    pickle.dump(player, file_out)
+    file_out.close()
 
-#function to load game
-def loadgame():
-    with open('savefile') as f:
-        pickle.load(f)
+#function to load game (WORKING)
+def loadgame(filename):
+    try:
+        file_in = open(filename, 'rb')
+    except:
+        print("Error opening save file")
 
-#to create ease for the user
+    res = pickle.load(file_in)
+    return res
+
+#Pygame image display function (WORKING)
+def disp_image(photo_file):
+    pygame.init()
+
+    carryOn = True
+ 
+    clock = pygame.time.Clock()
+    screen = pygame.display.set_mode((1400,1000))
+    # -------- Main Program Loop -----------
+    while carryOn:
+    # --- Main event loop
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: 
+                  carryOn = False 
+ 
+     # --- Game logic
+        loaded_photo = pygame.image.load(photo_file)
+        screen.blit(loaded_photo, (0,0))
+     # --- update the screen
+        pygame.display.flip()
+     
+     # --- Limit to 60 frames per second
+        clock.tick(60)
+ 
+    pygame.quit()
+
+
+#Start of rooms in dungeon crawl
+def room1():
+
+    disp_image("c:/Users/Jacob/OneDrive/Documents/Python homework/Final Project folder/First doors.PNG")
+
+    print(" ")
+    print("It was a dark and stormy night...")
+    print("I mean, of course it was, that's how all these things seem to start I guess...")
+    print("but it seems you don't really know that since you are deep underground in a dungeon of some kind.")
+    print(" ")
+    print("There are 3 doors in front of you in this barely lit room. To the left is a solid oak door. In front of you is a futuristic steel door, and to your right is an antique door with a brass knocker.")
+    print(" ")    
+    print("Through which door do you proceed?")
+    action = input("> ")
+    if action == "left":
+        room2()
+    elif action == "forward":
+        room3()
+    elif action == "right":
+        room4()
+    elif action == "save":
+        savegame('save.bin')
+        print("Saving completed,")
+        room1()
+    else:
+        print("Well you can't just sit there all day, or maybe you can, or maybe you'll starve, who knows.")
+        print("You should definitely enter just 'left', 'right', or 'forward'")
+        room1()
+
+def room2():
+    print("room 2")
+
+def room3():
+    print("room3")
+
+def room4():
+    print("room4")
+    
+
+
+#to create ease for the user  (Not working as of right now, much)
+
 left = ["Go Left", "go Left", "Go left", "go left", "Left", "left", "LEFT"]
 right = ["Go Right", "go Right", "Go right", "go right", "right", "Right", "RIGHT"]
 forward = ["Go Forward", "Go forward", "go Forward", "go forward", "Forward", "forward", "FORWARD", "forwards"]
